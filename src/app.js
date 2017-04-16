fefc25fd33bd23914517e0dd4db66964aa9e347b
Rishi Karri <rishikarri1@gmail.com>
2017-04-16 11:47:22 -0400

    added in common components

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers/index.js'
+import reducers from './reducers/index.js';
+import { Header } from './components/common/index.js';
 
 
 
 const App = () => {
 	return (
 		<Provider store={createStore(reducers)}> 
-			<View />
+			<View>
+				<Header headerText="Tech Stack" />
+			</View >
 		</Provider>
 	);
 };
