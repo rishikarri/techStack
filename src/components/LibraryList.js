278f7b91ba242dd8912c89db0000e3269fd76e1c
Rishi Karri <rishikarri1@gmail.com>
2017-04-16 13:01:47 -0400

    mapped librarylist to props

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,6 +1,6 @@
 // responsible for rendering list of libraries to the user 
-
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class LibraryList extends Component {
 	render() {
@@ -8,4 +8,10 @@ class LibraryList extends Component {
 	}
 }
 
-export default LibraryList;
+// purpose of the function is to take our global state object in our redux store, take them and provide them as props to our component in library list
+const mapStateToProps = state => {
+
+};
+export default connect()(LibraryList);
+
+
